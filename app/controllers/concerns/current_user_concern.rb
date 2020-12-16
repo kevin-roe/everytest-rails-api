b507f0882a69166852d6ad0e1abd80a95d2aa91e
Kevin Roe <kroe761@gmail.com>
2020-12-16 14:37:24 -0600

    it works!  removing logging

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,10 +6,6 @@ module CurrentUserConcern
   end
 
   def set_current_user
-    puts "############## " + "Session:::"
-    puts session[:user_id]
-    puts session[:organization_id]
-    puts "############## "
     if session[:user_id]
       @current_user = User.find(session[:user_id])
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,15 @@ class SessionsController < ApplicationController
     include CurrentUserConcern
   
     def create
-      puts "############## " + "Rails ENV: " + Rails.env
       user = User
               .find_by(email: params["email"])
               .try(:authenticate, params["password"])
   
       if user
-        puts "############## " + "Creating session"
         session[:user_id] = user.id
         session[:organization_id] = user.organization_id
-        puts session[:user_id]
-        puts session[:organization_id]
         render json: user.as_json(include: :organization, except: [:organization_id, :password_digest]), status: :created
       else
-        puts "############## " + "NOT AUTHORIZED"
         head :unauthorized
       end
     end
