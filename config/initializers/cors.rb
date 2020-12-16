4fe70ccbb6739c23b95cd37c0c902fda0af54a41
Kevin Roe <kroe761@gmail.com>
2020-12-16 14:27:34 -0600

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4200", "https://thawing-river-95379.herokuapp.com"
+    origins "http://localhost:4200", "https://thawing-river-95379.herokuapp.com", "http://kevinroe.me"
     resource "*", 
       headers: :any, 
       methods: [:get, :post, :put, :patch, :delete, :options, :head], 
