7b848bc9298ad7975f18c828506679434f88a870
Kevin Roe <kroe761@gmail.com>
2020-12-16 07:15:57 -0600

    cors and session

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,4 +8,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins "kevinroe.me"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
+
+    allow do
+      origins "https://thawing-river-95379.herokuapp.com/"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
   end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_everytest", domain: "kevinroe.me"
+    Rails.application.config.session_store :cookie_store, key: "_everytest", domain: "https://thawing-river-95379.herokuapp.com/"
   else
     Rails.application.config.session_store :cookie_store, key: "_everytest"
   end
