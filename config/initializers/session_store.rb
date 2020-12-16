156a0bc393524312c7d868bfb2fec063da4e22bc
Kevin Roe <kroe761@gmail.com>
2020-12-16 13:09:40 -0600

    session work

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_everytest", domain: "thawing-river-95379.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_everytest", domain: "thawing-river-95379.herokuapp.com", tld_length: 
 else
   Rails.application.config.session_store :cookie_store, key: "_everytest"
 end
