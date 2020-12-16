0f8ddc93cf7ab692dc4083cc099bb4815fc7951c
Kevin Roe <kroe761@gmail.com>
2020-12-16 14:22:01 -0600

    logging

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-Rails.application.config.session_store :cookie_store, key: "_everytest"
+Rails.application.config.session_store :cookie_store, key: "_everytest", domain: :all, tld_length: 2
 
 # if Rails.env == "production"
 #   Rails.application.config.session_store :cookie_store, key: "_everytest", domain: "thawing-river-95379.herokuapp.com", tld_length: 2
