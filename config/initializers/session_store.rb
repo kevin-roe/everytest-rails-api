de4f97bf4c02c7ba7774c31323885eac2986c688
Kevin Roe <kroe761@gmail.com>
2020-12-16 11:14:27 -0600

    sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_everytest", domain: "still-coast-96203.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_everytest", domain: :all, tld_length: 2
 else
   Rails.application.config.session_store :cookie_store, key: "_everytest"
 end
